58ec58c45cd21ddb1eae227d71d58308a311d3eb
lexfor <thetim182001@mail.ru>
2021-09-14 15:02:19 +0300

    Lesson-10: review fixes

--- src/api/requestProcessing/resolutions/getAllResolutionsByID/controller.js
+++ src/api/requestProcessing/resolutions/getAllResolutionsByID/controller.js
@@ -1,8 +1,8 @@
 import database from '../../../database/DatabaseStorage.js';
 
-export default async ({ data: doctorID }, res, next) => {
+export default async ({ params: { id: patientID } }, res, next) => {
   try {
-    res.send(await database.getAllResolutionsByID(doctorID));
+    res.send(await database.getAllResolutionsByID(patientID));
   } catch (error) {
     console.error(error);
     next(error);
--- src/api/requestProcessing/resolutions/getAllResolutionsByID/isValidParams.js
+++ src/api/requestProcessing/resolutions/getAllResolutionsByID/isValidParams.js
@@ -1,17 +1,11 @@
 import validator from 'validator';
 import ApiError from '../../../../errors/ApiError.js';
 
-class FindResolutionDTO {
-  constructor({ params: { id } } = {}) {
-    this.patientID = id;
-  }
-}
-
 const isValid = (patientID) => validator.isUUID(patientID);
 
 export default async (req, res, next) => {
   try {
-    const { patientID } = new FindResolutionDTO(req);
+    const { id: patientID } = req.params;
 
     if (!patientID) {
       throw new ApiError(400, 'Request searchString is empty');
@@ -21,7 +15,6 @@ export default async (req, res, next) => {
       throw new ApiError(400, 'searchString does not valid');
     }
 
-    req.data = patientID;
     next();
   } catch (error) {
     next(error);
